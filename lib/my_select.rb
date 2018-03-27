480c3c78f6d29bc8b205808331a424866feefd76
Joe Greiner <jrgreiner115@gmail.com>
2018-03-27 15:11:59 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+ if collection.length == 0
+return nil
+else
+  i = 0
+  new_array = []
+      while i < collection.length
+        if collection[i] % 2 == 0
+          yield collection[i]
+				  new_array << collection[i]
+              i += 1 
+        else
+          puts collection[i]
+				  i += 1
+end
+end
+new_array
+end
 end
